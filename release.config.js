d4c65ffcf6bb52387fc62d9e4ca31d0114091460
Leslie Enwerem <ifeanyi.enwerem@gmail.com>
2021-07-31 09:24:58 -0400

    chore: Code Formatting

--- release.config.js
+++ release.config.js
@@ -1,18 +1,18 @@
 // Semantic Release Config
 module.exports = {
-    branches: "master",
-    repositoryUrl: "https://github.com/goonerify/react-app",
-    plugins: [
-        // analyze commit messages and decide what the next version 
-        // should be based on the commit messages
-        "@semantic-release/commit-analyzer",
-        // Generates release notes for our release, also based on 
-        // our commit messages descriptions and types
-        "@semantic-release/release-notes-generator",
-        // This is used when releasing an npm package
-        // "@semantic-release/npm",
-        // This is responsible for creating the github release in the
-        // github releases page
-        "@semantic-release/github"
-    ]
-}
+  branches: "master",
+  repositoryUrl: "https://github.com/goonerify/react-app",
+  plugins: [
+    // analyze commit messages and decide what the next version
+    // should be based on the commit messages
+    "@semantic-release/commit-analyzer",
+    // Generates release notes for our release, also based on
+    // our commit messages descriptions and types
+    "@semantic-release/release-notes-generator",
+    // This is used when releasing an npm package
+    // "@semantic-release/npm",
+    // This is responsible for creating the github release in the
+    // github releases page
+    "@semantic-release/github"
+  ]
+};
